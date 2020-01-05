055b773ff539dfd4975000ae719248fc694197f1
Sahil Hadke <51915783+sahilhadke@users.noreply.github.com>
2020-01-05 17:50:32 +0530

    LineDrawing.cpp
    this program only calculates the coordinates.
    graphics.h needs to be included.
    graph needs to be initialized.

--- dda.cpp
+++ dda.cpp
@@ -1,65 +1,101 @@
 #include<iostream>
 #include<math.h>
-#include<graphics.h>
 using namespace std;
 
-template<typename T>
-int sign(T x){
-	if(x < 0)
-		return -1;
-	else if(x > 0)
-		return 1;
-	return 0;
+void DDA_Algo(int x1, int y1, int x2, int y2){
+
+    float dx = x2-x1, dy = y2 - y1;
+    int len;
+
+    float x = x1 + 0.5*(dx/abs((int)dx)), y = y1 + 0.5*(dy/abs((int)dy));
+
+    if(abs((int)dx) > abs((int)dy))
+        len = abs((int)dx);
+    else
+        len = abs((int)dy);
+
+    dx = (x2-x1)/(float)len, dy = (y2-y1)/(float)len;
+    cout<<dx<<" "<<dy<<endl<<endl<<endl;
+
+    for(int i = 0; i <= len; i++)
+    {
+        //putpixel(floor(x), floor(y));
+        cout<<floor(x)<<" "<<floor(y)<<endl;
+        x+=dx;
+        y+=dy;
+    }
 }
 
-int main(){
+void Bresenham_Algo(int x1, int y1, int x2, int y2){
 
-	float x1, x2, y1, y2, len, color = 255;
-	float x, y, dx, dy;
+    int dx = abs(x2 - x1), dy = abs(y2 - y1), x = x1, y = x1, len;
+    int e = (2*abs(dy))-dx;
 
-	cin>>x1>>y1>>x2>>y2;
 
-	dx = abs(x2-x1);
-	dy = abs(y2-y1);
+    if(abs(dx) > abs(dy))
+        len = abs(dx);
+    else
+        len = abs(dy);
 
-	if(dx > dy)
-		len = dx;
-	else
-		len = dy;
-	
+    for(int i = 0; i <= len; i++)
+    {
+        cout<<x<<" "<<y<<endl;
+        while(e >= 0)
+        {
+            y++;
+            e = e - 2*dx;
+        }
 
-	dx = (x2-x1)/len;
-	dy = (y2-y1)/len;
-
-	//int gd=DETECT,gm;
-	//initgraph(&gd,&gm,NULL);
-
-	int i = 0;
-
-	x = x1 + 0.5*(sign(dx));
-	y = y1 + 0.5*(sign(dy));	
-
-	dx = abs(dx);
-	dy = abs(dy);
-
-	if(dx < 0)
-		dx = dx * -1;
-	if(dy < 0)
-		dy = dy * -1;
-
-	while(i <= len){
-		//putpixel(floor(x), floor(y), color);
-		cout<<x<<" "<<y<<endl;
-		x+=dx;
-		y+=dy;
-		i++;
-	}
-
-	//getch();
-	//closegraph();
-
-	return 0;
+        x++;
+        e = e + 2*dy;
+    }
 }
 
+int main()
+{
 
-	
+    int x1, y1, x2, y2, choice, while_counter = 1;
+
+    while(while_counter)
+    {
+        cout<<"----MENU----"<<endl;
+        cout<<"1. DDA Algorithm"<<endl<<"2. Bresenham's Algorithm"<<endl<<"3. Exit"<<endl;
+        cout<<"Enter your choice :";
+        cin>>choice;
+
+        switch(choice)
+        {
+            case 1:
+                cout<<"Enter x1 :";
+                cin>>x1;
+                cout<<"Enter x1 :";
+                cin>>y1;
+                cout<<"Enter x1 :";
+                cin>>x2;
+                cout<<"Enter x1 :";
+                cin>>y2;
+
+                DDA_Algo(x1, y1, x2, y2);
+                break;
+
+            case 2:
+                cout<<"Enter x1 :";
+                cin>>x1;
+                cout<<"Enter x1 :";
+                cin>>y1;
+                cout<<"Enter x1 :";
+                cin>>x2;
+                cout<<"Enter x1 :";
+                cin>>y2;
+
+                Bresenham_Algo(x1, y1, x2, y2);
+                break;
+
+            default:
+                while_counter = 0;
+                break;
+        }
+    }
+
+    return 0;
+}
